5bc2023fcb72f16811e4d41aab33a45e25998f14
nilson junior <nilsonjunior@MacBook-Air-de-nilson.local>
2023-10-09 18:33:09 -0300

    atualizacoes com bootstrap

--- app.js
+++ app.js
@@ -4,13 +4,39 @@ const port = 4000;
 
 // Define a rota para a página HTML
 app.get('/livros', (req, res) => {
-  res.sendFile(__dirname + '/pages/livros.html');
+  res.sendFile(__dirname + '/pages/livros/listLivros.html');
 });
 
 // Define a rota para a página HTML
-app.get('/categorias', (req, res) => {
-  res.sendFile(__dirname + '/pages/categorias.html');
+app.get('/addlivros', (req, res) => {
+  res.sendFile(__dirname + '/pages/livros/livros.html');
 });
+
+// Define a rota para a página HTML
+app.get('/editlivros', (req, res) => {
+  res.sendFile(__dirname + '/pages/livros/editLivro.html');
+});
+
+
+
+
+// Define a rota para a página HTML
+app.get('/categorias', (req, res) => {
+  res.sendFile(__dirname + '/pages/categorias/listCategorias.html');
+});
+
+// Define a rota para a página HTML
+app.get('/addcategorias', (req, res) => {
+  res.sendFile(__dirname + '/pages/categorias/categorias.html');
+});
+
+// Define a rota para a página HTML
+app.get('/editcategorias', (req, res) => {
+  res.sendFile(__dirname + '/pages/categorias/editCategoria.html');
+});
+
+
+
 
 // Define a rota para a página HTML
 app.get('/usuarios', (req, res) => {
@@ -20,6 +46,11 @@ app.get('/usuarios', (req, res) => {
 // Define a rota para a página HTML
 app.get('/pedidos', (req, res) => {
   res.sendFile(__dirname + '/pages/pedidos.html');
+});
+
+// Define a rota para a página HTML
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/pages/index.html');
 });
 
 // Inicia o servidor
